dbd9ad799e59a71d123804492c9115a02cdc8d3e
Matt Rogers <matt.rogers8396@gmail.com>
2026-05-02 11:15:22 -0500

    removed bootstrap and started work on mainlayout

---
+++ src/OrdoWiki.Web/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace OrdoWiki.Web.Components.Layout;
+
+public partial class MainLayout
+{
+    private bool _drawerOpen;
+
+    private void ToggleDrawer()
+    {
+        _drawerOpen = !_drawerOpen;
+        Console.WriteLine("Toggled drawer");
+    }
+}
